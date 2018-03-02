6e1ef411c33e7ead2a13279aecadbbad16e46b22
jacobbogers <jkfbogers@gmail.com>
2018-03-02 01:59:58 +0100

    1 removed fortran files from src/lib

---
+++ src/lib/f_func.ts
@@ -0,0 +1,9 @@
+//intrinsic routines of fortran
+
+export function sign(a: number, b?: number): number {
+    if (b === undefined) {
+        return a;
+    }
+    const rc = Math.abs(a);
+    return b >= 0 ? rc : -rc;
+}
--- src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-import { srotg } from './srotg';
-import { srotmg } from './srotmg';
-
-export const level1 = {
-    srotg,
-    srotmg,
-    drotg: srotg,
-    drotmg: srotmg
-};
