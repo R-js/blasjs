babbdb1845bbab9c2e2d34f010f1d8eea0ae5132
jacobbogers <jkfbogers@gmail.com>
2018-03-13 19:23:28 +0100

    added double precision functions level2

--- src/lib/l2/index.ts
+++ src/lib/l2/index.ts
@@ -5,6 +5,7 @@ import { sger } from './single/sger';
 import { ssbmv } from './single/ssbmv';
 import { sspmv } from './single/sspmv';
 import { sspr } from './single/sspr';
+import { sspr2 } from './single/sspr2';
 import { ssymv } from './single/ssymv';
 import { ssyr } from './single/ssyr';
 import { ssyr2 } from './single/ssyr2';
@@ -17,20 +18,40 @@ import { strsv } from './single/strsv';
 
 export const level2 = {
     //single
-    sgbmv,
+    sgbmv, //1
     sgemv,
     sger,
     ssbmv,
-    sspmv,
+    sspmv, //5
     sspr,
+    sspr2,
     ssymv,
     ssyr,
-    ssyr2,
+    ssyr2, //10
     stbmv,
     stbsv,
     stpmv,
     stpsv,
-    strmv,
-    strsv
+    strmv, //15
+    strsv,
+    // double precision
+    dgbmv: sgbmv, //1
+    dgemv: sgemv,
+    dger: sger,
+    dsbmv: ssbmv,
+    dspmv: sspmv, //5
+    dspr: sspr,
+    dspr2: sspr2,
+    dsymv: ssymv,
+    dsyr: ssyr,
+    dsyr2: ssyr2, //10
+    dtbmv: stbmv,
+    dtbsv: stbsv,
+    dtpmv: stpmv,
+    dtpsv: stpsv,
+    dtrmv: strmv, //15
+    dtrsv: strsv,
+    //complex single
+
 };
 
